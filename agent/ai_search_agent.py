6ead93b53a374d0655bef3a7497c9d8061ada006
pulseagentai <pulseagentai@gmail.com>
2025-01-08 23:45:33 +0800

    react

--- agent/ai_search_agent.py
+++ agent/ai_search_agent.py
@@ -1,2 +1,11 @@
+from langchain_openai import ChatOpenAI
+from langgraph.prebuilt import create_react_agent
+
+
 async def ai_search_agent(query: str):
-    pass
+    model = ChatOpenAI(model="gpt-4o", temperature=0.01)
+    tools = [
+
+    ]
+    graph = create_react_agent(model, tools=tools)
+    return graph.astream({"query": query})
