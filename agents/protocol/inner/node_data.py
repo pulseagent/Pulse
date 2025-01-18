be42894fae6e991c50718558e8865fba221b74aa
use-none <nonegentai@gmail.com>
2025-01-18 23:39:48 +0800

    feat: add planning node log push

---
+++ agents/protocol/inner/node_data.py
@@ -0,0 +1,19 @@
+"""NodeMessage Class"""
+from typing import Optional
+
+
+class NodeMessage:
+    message: str = ""
+    tool_name: str = ""
+
+    def __init__(self, message: str, tool_name: Optional[str]=None):
+        """Constructor for the class"""
+        self.message = message
+        self.tool_name = tool_name
+
+    def to_dict(self):
+        data = {}
+        for key, value in self.__dict__.items():
+            if value:
+                data[key] = data
+        return data
